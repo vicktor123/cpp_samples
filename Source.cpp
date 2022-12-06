e714753c9ee0c736007610abc14a66f9153994f2
help <ru@rus.ru>
2022-12-06 09:47:08 +0300

    4 commit

--- Source.cpp
+++ Source.cpp
@@ -20,8 +20,9 @@ void NewFunction(char  str[100], char temp)
 	char* pos = strchr(str, temp);
 	int start = pos - str;
 	int c = 1;
-	while (*(pos) != '\0' && c<2) {
+	while (c<2) {
 		pos = strchr(pos + 1, temp);
+		if (pos == NULL) return;
 		if (*pos == temp) c++;
 	}
 	int end = pos - str;
