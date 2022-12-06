0ee19b8330777b8956a8aa7616b90ef6d9e0ec9d
help <ru@rus.ru>
2022-12-06 09:32:11 +0300

    3 commit

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include "Source.h"
 using namespace std;
 const int SIZE = 100;
 
@@ -10,10 +11,16 @@ int main() {
 
 	char temp = '#';
 
+	NewFunction(str, temp);
+	return 0;
+}
+
+void NewFunction(char  str[100], char temp)
+{
 	char* pos = strchr(str, temp);
-	int start= pos-str;
+	int start = pos - str;
 	int c = 1;
-	while (*(pos)!='\0' && c<2) {
+	while (*(pos) != '\0' && c<2) {
 		pos = strchr(pos + 1, temp);
 		if (*pos == temp) c++;
 	}
@@ -21,9 +28,8 @@ int main() {
 	char* newstr = new char[end - start];
 	int k = 0;
 	for (int i = start + 1;i < end;i++)
-		newstr[k++]=str[i];
-	newstr[end - start-1] = '\0';
+		newstr[k++] = str[i];
+	newstr[end - start - 1] = '\0';
 	cout << newstr << endl;
 	delete[] newstr;
-	return 0;
 }
