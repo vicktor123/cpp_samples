90df0993779156d63f1d238fab485d1356e8f52b
help <ru@rus.ru>
2022-12-06 10:22:14 +0300

    6 commit

---
+++ Source3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include "Source.h"
+using namespace std;
+const int SIZE = 100;
+
+int main() {
+
+	char str[SIZE];
+	cin.getline(str, 100);
+
+	char temp = 'b';
+
+	NewFunction(str, temp);
+	return 0;
+}
+
+void NewFunction(char  str[100], char temp)
+{
+	int e = 0;
+	char* newstr = new char[strlen(str)];
+	for (int i = 0;i < strlen(str);i++) {
+		
+		if (str[i] == temp) continue;
+		newstr[e++] = str[i];
+	}
+		
+	newstr[e] = '\0';
+	cout << newstr << endl;
+	delete[] newstr;
+}
